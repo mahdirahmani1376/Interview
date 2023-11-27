4b143c35840700d7e5c30bf1510919c24e295bda
mahdirahmani1376 <rahmanimahdi20@yahoo.com>
2023-11-27 23:11:29 +0330

    installing jwt and add login and controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
+
+Route::prefix('auth')->controller(AuthController::class)->middleware('api')->group(function (){
+   Route::post('/login','login')->name('auth.login');
+   Route::post('/logout','logout')->name('auth.logout');
+   Route::post('/refresh','refresh')->name('auth.refresh');
+   Route::post('/me','me')->name('auth.me');
 });
